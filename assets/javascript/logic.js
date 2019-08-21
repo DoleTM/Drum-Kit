1d0dc7a6107c4c527c6bae6ba6b1eb726978f331
DoleTM <adylantoremember@gmail.com>
2019-08-21 19:44:08 -0400

    Functional no style

---
+++ assets/javascript/logic.js
@@ -0,0 +1,19 @@
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) return; // will return no audio
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+
+});
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach( key => key.addEventListener('transitioned', removeTransition));
